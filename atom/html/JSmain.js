aab06122db2458ec4c5ff98873e54ee71ad487ce
emrys <filippoferrian@gmail.com>
2024-10-09 10:06:52 +0200

    new file:   atom/html/JSes7.2.zip 	modified:   atom/html/JSmain.js 	modified:   atom/html/main.html 	new file:   atom/jScripts/threads/Campanai.class

--- atom/html/JSmain.js
+++ atom/html/JSmain.js
@@ -1,18 +1,65 @@
 let APP = {
-    callback_scrivi : function(){
+    callback_global: function (a) {
+        let op1 = parseInt($("#op1").val());
+        let op2 = parseInt($("#op2").val());
+        let res;
+        switch (a.valueOf()) {
+            case 1:
+                res = op1 + op2;
+                break;
+            case 2:
+                res = op1 - op2;
+                break;
+            case 3:
+                res = op1 * op2;
+                break;
+            case 4:
+                res = op1 / op2;
+                break;
+        }
+        $("#text").text(res);
+    },
+    callback_addizione: function () {
         let op1 = parseInt($("#op1").val());
         let op2 = parseInt($("#op2").val());
         let res = op1 + op2;
         $("#text").text(res);
     },
+    callback_sottrazione: function () {
+        let op1 = parseInt($("#op1").val());
+        let op2 = parseInt($("#op2").val());
+        let res = op1 - op2;
+        $("#text").text(res);
+    },
+    callback_moltiplicazione: function () {
+        let op1 = parseInt($("#op1").val());
+        let op2 = parseInt($("#op2").val());
+        let res = op1 * op2;
+        $("#text").text(res);
+    },
+    callback_divisione: function () {
+        let op1 = parseInt($("#op1").val());
+        let op2 = parseInt($("#op2").val());
+        let res = op1 / op2;
+        $("#text").text(res);
+    },
 
-    callback_scrivi_click : function(){
-        $("#btn").on('click', APP.callback_scrivi);
+    callback_scrivi_click: function () {
+/*
+        $("#add").on('click', APP.callback_global(1));
+        $("#min").on('click', APP.callback_global(2));
+        $("#molt").on('click', APP.callback_global(3));
+        $("#div").on('click', APP.callback_global(4));
+*/
+        $("#add").on('click', APP.callback_addizione);
+        $("#min").on('click', APP.callback_sottrazione);
+        $("#molt").on('click', APP.callback_moltiplicazione);
+        $("#div").on('click', APP.callback_divisione);
     }
 };
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     APP.callback_scrivi_click();
 }
 );
