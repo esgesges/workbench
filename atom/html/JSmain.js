57b9c657898788f45907018b0778e47b4d80274f
emrys <filippoferrian@gmail.com>
2024-11-06 09:14:43 +0100

    modified:   atom/html/CSSmain.css 	modified:   atom/html/JSmain.js 	new file:   atom/html/Jses9.zip 	new file:   atom/html/ferrian.zip 	modified:   atom/html/main.html 	deleted:    atom/pyScripts/README.md

--- atom/html/JSmain.js
+++ atom/html/JSmain.js
@@ -1,65 +1,68 @@
 let APP = {
-    callback_global: function (a) {
-        let op1 = parseInt($("#op1").val());
-        let op2 = parseInt($("#op2").val());
-        let res;
-        switch (a.valueOf()) {
-            case 1:
-                res = op1 + op2;
-                break;
-            case 2:
-                res = op1 - op2;
-                break;
-            case 3:
-                res = op1 * op2;
-                break;
-            case 4:
-                res = op1 / op2;
-                break;
-        }
-        $("#text").text(res);
-    },
-    callback_addizione: function () {
-        let op1 = parseInt($("#op1").val());
-        let op2 = parseInt($("#op2").val());
-        let res = op1 + op2;
-        $("#text").text(res);
-    },
-    callback_sottrazione: function () {
-        let op1 = parseInt($("#op1").val());
-        let op2 = parseInt($("#op2").val());
-        let res = op1 - op2;
-        $("#text").text(res);
-    },
-    callback_moltiplicazione: function () {
-        let op1 = parseInt($("#op1").val());
-        let op2 = parseInt($("#op2").val());
-        let res = op1 * op2;
-        $("#text").text(res);
-    },
-    callback_divisione: function () {
-        let op1 = parseInt($("#op1").val());
-        let op2 = parseInt($("#op2").val());
-        let res = op1 / op2;
-        $("#text").text(res);
+    ordGiusto: ["1", "2", "3", "4", "5", "6", "7", "8", "n"],
+    ordCorrente: ["1", "2", "3", "4", "5", "6", "7", "n", "8"],
+    start: function(e){
+        $("button").on('click', APP.sposta)
     },
 
-    callback_scrivi_click: function () {
-/*
-        $("#add").on('click', APP.callback_global(1));
-        $("#min").on('click', APP.callback_global(2));
-        $("#molt").on('click', APP.callback_global(3));
-        $("#div").on('click', APP.callback_global(4));
-*/
-        $("#add").on('click', APP.callback_addizione);
-        $("#min").on('click', APP.callback_sottrazione);
-        $("#molt").on('click', APP.callback_moltiplicazione);
-        $("#div").on('click', APP.callback_divisione);
+    sposta: function(e){
+        let numClickato = $(e.target);
+        let pos = APP.ordCorrente.indexOf(numClickato.text());
+    
+        if (pos !== -1 && APP.ordCorrente[pos + 1] === "n" && (pos + 1) % 3 !== 0) {
+            APP.ordCorrente[pos + 1] = APP.ordCorrente[pos];
+            APP.ordCorrente[pos] = "n";
+            APP.set();
+        } else if (pos !== -1 && APP.ordCorrente[pos - 1] === "n" && pos % 3 !== 0) {
+            APP.ordCorrente[pos - 1] = APP.ordCorrente[pos];
+            APP.ordCorrente[pos] = "n";
+            APP.set();
+        } else if (pos !== -1 && APP.ordCorrente[pos + 3] === "n") {
+            APP.ordCorrente[pos + 3] = APP.ordCorrente[pos];
+            APP.ordCorrente[pos] = "n";
+            APP.set();
+        } else if (pos !== -1 && APP.ordCorrente[pos - 3] === "n") {
+            APP.ordCorrente[pos - 3] = APP.ordCorrente[pos];
+            APP.ordCorrente[pos] = "n";
+            APP.set();
+        } else {
+            console.log("non puoi spostarlo");
+        }
+        APP.checkWin();
+    },
+
+    set: function(){
+        $("#1").text(APP.ordCorrente[0]);
+        $("#1").val(APP.ordCorrente[0]);
+        $("#2").text(APP.ordCorrente[1]);
+        $("#2").val(APP.ordCorrente[1]);
+        $("#3").text(APP.ordCorrente[2]);
+        $("#3").val(APP.ordCorrente[2]);
+        $("#4").text(APP.ordCorrente[3]);
+        $("#4").val(APP.ordCorrente[3]);
+        $("#5").text(APP.ordCorrente[4]);
+        $("#5").val(APP.ordCorrente[4]);
+        $("#6").text(APP.ordCorrente[5]);
+        $("#6").val(APP.ordCorrente[5]);
+        $("#7").text(APP.ordCorrente[6]);
+        $("#7").val(APP.ordCorrente[6]);
+        $("#8").text(APP.ordCorrente[7]);
+        $("#8").val(APP.ordCorrente[7]);
+        $("#9").text(APP.ordCorrente[8]);
+        $("#9").val(APP.ordCorrente[8]);
+    },
+
+    checkWin: function(){
+        if (APP.ordCorrente.join("") === APP.ordGiusto.join("")) {
+            alert("Hai vinto!");
+        }
     }
-};
+
+}
 
 
 $(document).ready(function () {
-    APP.callback_scrivi_click();
+    APP.start();
+    APP.set();
 }
 );
